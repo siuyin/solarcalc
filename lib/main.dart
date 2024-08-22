09d2b356f12c5b2b2e07a5817871e8733017adbb
Loh Siu Yin <siuyin@beyondbroadcast.com>
2024-08-22 20:57:34 +0800

    refactor: class names

--- lib/main.dart
+++ lib/main.dart
@@ -58,243 +58,23 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: ListView(
         children: const [
-          Budget(),
-          Solar(),
-          Utility(),
+          EnergyBudget(),
+          SolarCable(),
+          BatteryCable(),
         ],
       ),
     );
   }
 }
 
-class Utility extends StatefulWidget {
-  const Utility({super.key});
+class EnergyBudget extends StatefulWidget {
+  const EnergyBudget({super.key});
 
   @override
-  State<Utility> createState() => _UtilityState();
+  State<EnergyBudget> createState() => _EnergyBudgetState();
 }
 
-class _UtilityState extends State<Utility> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 32),
-      child: const Text('Utility'),
-    );
-  }
-}
-
-class Solar extends StatefulWidget {
-  const Solar({super.key});
-
-  @override
-  State<Solar> createState() => _SolarState();
-}
-
-class _SolarState extends State<Solar> {
-  dynamic box;
-  String outputText = '';
-
-  @override
-  void initState() {
-    super.initState();
-    _init();
-  }
-
-  setIfEmpty(String key, dynamic value) {
-    if (box.get(key) != null) return;
-    box.put(key, value);
-    debugPrint('set $key to $value');
-  }
-
-  _init() async {
-    await Hive.initFlutter();
-    box = await Hive.openBox('solar');
-    setIfEmpty('cableCrossSection', 2.5);
-    setState(() {
-      cableCrossSection = box.get('cableCrossSection');
-    });
-
-    setIfEmpty('cableLength', cableLength);
-    cableLengthController.text = box.get('cableLength').toString();
-
-    setIfEmpty('cableTemp', cableTemp);
-    cableTempController.text = box.get('cableTemp').toString();
-
-    setIfEmpty('numConductors', numConductors);
-    numConductorsController.text = box.get('numConductors').toString();
-
-    setIfEmpty('iSC', iSC);
-    iSCController.text = box.get('iSC').toString();
-
-    box.put('gerbau', 'terpau');
-  }
-
-  static final list = <(String, double)>[
-    ('1mm²', 1.0),
-    ('1.5mm²', 1.5),
-    ('2.5mm²', 2.5),
-    ('4mm²', 4.0),
-    ('16AWG', 1.31),
-    ('14AWG', 2.08),
-    ('12AWG', 3.31),
-    ('10AWG', 5.26),
-  ];
-  double cableCrossSection = list.first.$2;
-  double cableTemp = 30.0;
-  double cableLength = 10.0;
-  int numConductors = 2;
-  double iSC = 5.5;
-
-  final cableTempController = TextEditingController();
-  final cableLengthController = TextEditingController();
-  final numConductorsController = TextEditingController();
-  final iSCController = TextEditingController();
-
-  double temperatureCorrectedCopperResistivity() {
-    return 17.24e-9 * (1 + 3.93e-3 * (cableTemp - 20.0));
-  }
-
-  double singleConductorResistance() {
-    return temperatureCorrectedCopperResistivity() *
-        cableLength /
-        cableCrossSection *
-        1e6;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Divider(),
-        Text(
-          'Solar Cable',
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
-        Row(
-          // crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            DropdownButton(
-              value: cableCrossSection,
-              items: list.map<DropdownMenuItem>((val) {
-                return DropdownMenuItem(
-                  value: val.$2,
-                  child: Text(val.$1),
-                );
-              }).toList(),
-              onChanged: (val) {
-                setState(() {
-                  cableCrossSection = val!;
-                  compute();
-                });
-              },
-              padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-            ),
-            SizedBox(
-              width: 64,
-              child: TextField(
-                controller: cableLengthController,
-                onSubmitted: (_) => compute(),
-                keyboardType: TextInputType.number,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                decoration: const InputDecoration(
-                  labelText: 'Length (m)',
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 64,
-              child: TextField(
-                controller: cableTempController,
-                onSubmitted: (_) => compute(),
-                keyboardType: TextInputType.number,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                decoration: const InputDecoration(
-                  labelText: '°C',
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 32,
-              child: TextField(
-                controller: numConductorsController,
-                onSubmitted: (_) => compute(),
-                keyboardType: TextInputType.number,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                decoration: const InputDecoration(
-                  labelText: 'num.',
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 64,
-              child: TextField(
-                controller: iSCController,
-                onSubmitted: (_) => compute(),
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(
-                    RegExp(r'[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)'),
-                  )
-                ],
-                decoration: const InputDecoration(
-                  labelText: 'Isc',
-                  hintText: 'Panel sort-circuit current',
-                ),
-              ),
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(outputText),
-              ),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-
-  double powerLoss(double i, double r, int n) {
-    return i * i * r * n;
-  }
-
-  compute() {
-    setState(() {
-      box.put('cableCrossSection', cableCrossSection);
-
-      cableLength = double.tryParse(cableLengthController.text) ?? cableLength;
-      box.put('cableLength', cableLength);
-
-      cableTemp = double.tryParse(cableTempController.text) ?? cableTemp;
-      box.put('cableTemp', cableTemp);
-
-      numConductors =
-          int.tryParse(numConductorsController.text) ?? numConductors;
-      box.put('numConductors', numConductors);
-
-      iSC = double.tryParse(iSCController.text) ?? iSC;
-      box.put('iSC', iSC);
-
-      outputText =
-          'Single ${cableLength}m conductor resistance:  ${singleConductorResistance().toStringAsPrecision(3)}Ω'
-          '\nGiven panel Isc: ${iSC}A, power loss for $numConductors conductors: ${powerLoss(box.get('iSC'), singleConductorResistance(), numConductors).toStringAsFixed(1)}W';
-    });
-  }
-}
-
-class Budget extends StatefulWidget {
-  const Budget({super.key});
-
-  @override
-  State<Budget> createState() => _BudgetState();
-}
-
-class _BudgetState extends State<Budget> {
+class _EnergyBudgetState extends State<EnergyBudget> {
   int numPanels = 1;
   double panelPower = 100;
   double panelOCV = 22.1;
@@ -674,4 +454,224 @@ class _BudgetState extends State<Budget> {
   }
 }
 
-hiveDemo() async {}
+class SolarCable extends StatefulWidget {
+  const SolarCable({super.key});
+
+  @override
+  State<SolarCable> createState() => _SolarCableState();
+}
+
+class _SolarCableState extends State<SolarCable> {
+  dynamic box;
+  String outputText = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  setIfEmpty(String key, dynamic value) {
+    if (box.get(key) != null) return;
+    box.put(key, value);
+    debugPrint('set $key to $value');
+  }
+
+  _init() async {
+    await Hive.initFlutter();
+    box = await Hive.openBox('solar');
+    setIfEmpty('cableCrossSection', 2.5);
+    setState(() {
+      cableCrossSection = box.get('cableCrossSection');
+    });
+
+    setIfEmpty('cableLength', cableLength);
+    cableLengthController.text = box.get('cableLength').toString();
+
+    setIfEmpty('cableTemp', cableTemp);
+    cableTempController.text = box.get('cableTemp').toString();
+
+    setIfEmpty('numConductors', numConductors);
+    numConductorsController.text = box.get('numConductors').toString();
+
+    setIfEmpty('iSC', iSC);
+    iSCController.text = box.get('iSC').toString();
+
+    box.put('gerbau', 'terpau');
+  }
+
+  static final list = <(String, double)>[
+    ('1mm²', 1.0),
+    ('1.5mm²', 1.5),
+    ('2.5mm²', 2.5),
+    ('4mm²', 4.0),
+    ('16AWG', 1.31),
+    ('14AWG', 2.08),
+    ('12AWG', 3.31),
+    ('10AWG', 5.26),
+  ];
+  double cableCrossSection = list.first.$2;
+  double cableTemp = 30.0;
+  double cableLength = 10.0;
+  int numConductors = 2;
+  double iSC = 5.5;
+
+  final cableTempController = TextEditingController();
+  final cableLengthController = TextEditingController();
+  final numConductorsController = TextEditingController();
+  final iSCController = TextEditingController();
+
+  double temperatureCorrectedCopperResistivity() {
+    return 17.24e-9 * (1 + 3.93e-3 * (cableTemp - 20.0));
+  }
+
+  double singleConductorResistance() {
+    return temperatureCorrectedCopperResistivity() *
+        cableLength /
+        cableCrossSection *
+        1e6;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Divider(),
+        Text(
+          'Solar Cable',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        Row(
+          // crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            DropdownButton(
+              value: cableCrossSection,
+              items: list.map<DropdownMenuItem>((val) {
+                return DropdownMenuItem(
+                  value: val.$2,
+                  child: Text(val.$1),
+                );
+              }).toList(),
+              onChanged: (val) {
+                setState(() {
+                  cableCrossSection = val!;
+                  compute();
+                });
+              },
+              padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+            ),
+            SizedBox(
+              width: 64,
+              child: TextField(
+                controller: cableLengthController,
+                onSubmitted: (_) => compute(),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: const InputDecoration(
+                  labelText: 'Length (m)',
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 64,
+              child: TextField(
+                controller: cableTempController,
+                onSubmitted: (_) => compute(),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: const InputDecoration(
+                  labelText: '°C',
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 32,
+              child: TextField(
+                controller: numConductorsController,
+                onSubmitted: (_) => compute(),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: const InputDecoration(
+                  labelText: 'num.',
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 64,
+              child: TextField(
+                controller: iSCController,
+                onSubmitted: (_) => compute(),
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(
+                    RegExp(r'[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)'),
+                  )
+                ],
+                decoration: const InputDecoration(
+                  labelText: 'Isc',
+                  hintText: 'Panel sort-circuit current',
+                ),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(outputText),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  double powerLoss(double i, double r, int n) {
+    return i * i * r * n;
+  }
+
+  compute() {
+    setState(() {
+      box.put('cableCrossSection', cableCrossSection);
+
+      cableLength = double.tryParse(cableLengthController.text) ?? cableLength;
+      box.put('cableLength', cableLength);
+
+      cableTemp = double.tryParse(cableTempController.text) ?? cableTemp;
+      box.put('cableTemp', cableTemp);
+
+      numConductors =
+          int.tryParse(numConductorsController.text) ?? numConductors;
+      box.put('numConductors', numConductors);
+
+      iSC = double.tryParse(iSCController.text) ?? iSC;
+      box.put('iSC', iSC);
+
+      outputText =
+          'Single ${cableLength}m conductor resistance:  ${singleConductorResistance().toStringAsPrecision(3)}Ω'
+          '\nGiven panel Isc: ${iSC}A, power loss for $numConductors conductors: ${powerLoss(box.get('iSC'), singleConductorResistance(), numConductors).toStringAsFixed(1)}W';
+    });
+  }
+}
+
+
+
+class BatteryCable extends StatefulWidget {
+  const BatteryCable({super.key});
+
+  @override
+  State<BatteryCable> createState() => _BatteryCableState();
+}
+
+class _BatteryCableState extends State<BatteryCable> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 32),
+      child: const Text('Utility'),
+    );
+  }
+}
